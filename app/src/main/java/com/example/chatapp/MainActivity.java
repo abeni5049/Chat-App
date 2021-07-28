7ca22579043c3917d983c23b61b9bca19a580a44
abeni <abeni5049@gmail.com>
2021-07-28 16:53:43 +0300

    display last message on the list

--- app/src/main/java/com/example/chatapp/MainActivity.java
+++ app/src/main/java/com/example/chatapp/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.chatapp;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,6 +18,7 @@ import com.google.firebase.database.ValueEventListener;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,25 +39,24 @@ public class MainActivity extends AppCompatActivity {
 
 
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        String uid = "1pUiLVhyzseX4Q3b9HwQd4P4NL12";
-        String uid2 = "ovPshVxVx2OXcOa0mFJP5m6345v1";
-        DatabaseReference myRef = database.getReference("users").child(uid).child("chats").child(uid2).child("username");
-        DatabaseReference myRef2 = database.getReference("users").child(uid2).child("chats").child(uid).child("username");
-        myRef.setValue("Iris");
-        myRef2.setValue("Abeni");
-
+        DatabaseReference myRef = database.getReference("users").child(LogInActivity.user.getUid()).child("chats");
         ListViewAdapter adapter = new ListViewAdapter(this,userName,subtitle,profile);
         list = findViewById(R.id.list);
         list.setAdapter(adapter);
-
-        myRef.addValueEventListener(new ValueEventListener() {
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull @NotNull DataSnapshot snapshot) {
-                String value = snapshot.getValue(String.class);
-                userName.add(value);
-                subtitle.add("hi");
-                profile.add(value.charAt(0));
-                adapter.notifyDataSetChanged();
+                for (DataSnapshot child : snapshot.getChildren()) {
+                    String username = child.child("username").getValue().toString();
+                    String lastMessage="";
+                    for( DataSnapshot snap  :child.child("messages").getChildren() ){
+                        lastMessage = Objects.requireNonNull(snap.child("text").getValue()).toString();
+                    }
+                    userName.add(username);
+                    subtitle.add(lastMessage);
+                    profile.add(username.charAt(0));
+                    adapter.notifyDataSetChanged();
+                }
             }
 
             @Override
